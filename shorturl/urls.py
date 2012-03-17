5ff59f81c20e28c75b11a702b33bc0d8f7a3900c
Zainan Victor Zhou <zzn@zzn.im>
2012-03-17 02:30:38 -0400

    v1.1.0 added redirect function

--- shorturl/urls.py
+++ shorturl/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     
-    #url(r'^(?P<short>.+)/$', 'shorturl.views.short_url_redirect'),
+    url(r'^(?P<short>.+)/$', 'shorturl.views.short_url_redirect'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
